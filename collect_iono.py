d4ea0a2bcc5d8b743052f9ab09c1c43398adcb10
Juha Vierinen <jvierine@gmail.com>
2020-11-23 20:02:38 +0000

    added ionogram data collector

--- collect_iono.py
+++ collect_iono.py
@@ -3,8 +3,10 @@ import h5py
 import numpy as n
 
 fl=glob.glob("iono*.h5")
+n.random.shuffle(fl)
 data=[]
-labels=[]
+ok_data=[]
+scaling=[]
 ok_labels=[]
 n_ok=0
 n_bad=0
@@ -21,27 +23,38 @@ for f in fl:
     if muf > 0.0:
         pf=1.0
 
-    ok=1
+    ok=0
     if hmf>0.0 and muf > 0.0:
-        n_ok+=1
+        ok=1
+        scaling.append(n.array([hmf,muf]))
+        ok_data.append(n.copy(h["img"].value))        
     else:
-        n_bad+=1
         ok=0
-    if n_ok >= n_bad-10 and ok == 0:
+        
+    if ok==1 or (n_ok >= (n_bad-10)):
+        if ok == 1:
+            n_ok+=1
+        else:
+            n_bad+=1
+        print("adding %d"%(ok))
         data.append(n.copy(h["img"].value))
-        labels.append(n.array([hmf,muf,ph,pf]))
         ok_labels.append(ok)
+    
     else:
-        print("ok %d not adding bad example"%(ok))
+        print("ok %d not adding bad example n_ok %d n_bad %d"%(ok,n_ok,n_bad))
         
     h.close()
     
 data=n.array(data,dtype=n.uint8)
-labels=n.array(labels)
+ok_data=n.array(ok_data,dtype=n.uint8)
+ok_labels=n.array(ok_labels)
+scaling=n.array(scaling)
 
 ho=h5py.File("all.h5","w")
 ho["data"]=data
-ho["labels"]=labels
+ho["ok_data"]=ok_data
+ho["scaling"]=scaling
+ho["ok"]=ok_labels
 ho.close()
 
 
