a10d2b650fa2163f9960f580ac94b77326d6beb5
juhav <juha-pekka.vierinen@uit.no>
2020-11-23 13:56:48 +0000

    added filter

--- collect_iono.py
+++ collect_iono.py
@@ -5,21 +5,37 @@ import numpy as n
 fl=glob.glob("iono*.h5")
 data=[]
 labels=[]
+ok_labels=[]
+n_ok=0
+n_bad=0
 for f in fl:
     h=h5py.File(f,"r")
-    data.append(n.copy(h["img"].value))
+
     ph=0.0
     pf=0.0
     hmf=n.copy(h["hmf"].value)
     muf=n.copy(h["muf"].value)
-    print(hmf)
-    print(muf)
+
     if hmf > 0.0:
         ph=1.0
     if muf > 0.0:
         pf=1.0
-    labels.append(n.array([hmf,muf,ph,pf]))
+
+    ok=1
+    if hmf>0.0 and muf > 0.0:
+        n_ok+=1
+    else:
+        n_bad+=1
+        ok=0
+    if n_ok >= n_bad-10 and ok == 0:
+        data.append(n.copy(h["img"].value))
+        labels.append(n.array([hmf,muf,ph,pf]))
+        ok_labels.append(ok)
+    else:
+        print("ok %d not adding bad example"%(ok))
+        
     h.close()
+    
 data=n.array(data,dtype=n.uint8)
 labels=n.array(labels)
 
